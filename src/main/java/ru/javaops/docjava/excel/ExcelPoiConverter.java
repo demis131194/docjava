879c58465042af71d8c1459594e593a75f523c7d
demis <demis131194@gmail.com>
2026-01-28 20:18:26 +0100

    20_poi_placeholders

--- src/main/java/ru/javaops/docjava/excel/ExcelPoiConverter.java
+++ src/main/java/ru/javaops/docjava/excel/ExcelPoiConverter.java
@@ -1,22 +1,46 @@
 package ru.javaops.docjava.excel;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExcelPoiConverter {
+    private static final Pattern PLACEHOLDER_PATTERN = Pattern.compile("\\$\\{(\\w+)}");
     private final File templateFile;
+    private final List<Placeholder> placeholders = new ArrayList<>();
 
-    public static ExcelPoiConverter of(File templateFile) {
+    public record Placeholder(Matcher matcher, CellAddress cellAddress) {
+    }
+
+    public static ExcelPoiConverter of(File templateFile) throws IOException {
         return new ExcelPoiConverter(templateFile);
     }
 
-    private ExcelPoiConverter(File templateFile) {
+    private ExcelPoiConverter(File templateFile) throws IOException {
         this.templateFile = templateFile;
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream((templateFile)))) {
+            // find all placeholders on sheet 0
+            workbook.getSheetAt(0).forEach(row ->
+                    row.forEach(cell -> {
+                        Matcher matcher = PLACEHOLDER_PATTERN.matcher(cell.getStringCellValue());
+                        if (matcher.find()) {
+                            placeholders.add(new Placeholder(matcher, cell.getAddress()));
+                        }
+                    }));
+        }
     }
 
     public void convert(File outputFile, Consumer<Workbook> workbookComposer) throws IOException {
@@ -28,5 +52,28 @@ public class ExcelPoiConverter {
                 workbook.write(outputStream);
             }
         }
+    }
+
+    public void resolvePlaceholders(Sheet sheet, Map<String, Supplier<Object>> supplierMap) {
+        placeholders.forEach(p -> {
+            p.matcher.reset();
+            // https://stackoverflow.com/questions/38376584/548473
+            StringBuilder sb = new StringBuilder();
+            while (p.matcher.find()) {
+                String key = p.matcher.group(1);
+                Object obj = supplierMap.get(key).get();
+                p.matcher.appendReplacement(sb, obj.toString());
+            }
+            p.matcher.appendTail(sb);
+            setCell(sheet, p.cellAddress, sb.toString());
+        });
+    }
+
+    public static Cell getCell(Sheet sheet, CellAddress cellAddress) {
+        return sheet.getRow(cellAddress.getRow()).getCell(cellAddress.getColumn());
+    }
+
+    public static void setCell(Sheet sheet, CellAddress cellAddress, Object value) {
+        getCell(sheet, cellAddress).setCellValue(value.toString());
     }
 }
--- src/main/java/ru/javaops/docjava/excel/ExcelPoiUtil.java
+++ src/main/java/ru/javaops/docjava/excel/ExcelPoiUtil.java
@@ -2,6 +2,7 @@ package ru.javaops.docjava.excel;
 
 import jakarta.xml.bind.JAXBException;
 import lombok.experimental.UtilityClass;
+import org.apache.poi.ss.usermodel.Sheet;
 import ru.javaops.docjava.schema.User;
 import ru.javaops.docjava.xml.jaxb.JaxbUtil;
 
@@ -10,17 +11,29 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import static ru.javaops.docjava.util.Util.DATE_TIME_FORMATTER;
+import static ru.javaops.docjava.util.Util.toDateTime;
+
 @UtilityClass
 public class ExcelPoiUtil {
-    public void convert(File inputFile, File templateFile, File outputFile, Map<String, Object> params) throws JAXBException, IOException {
+    public void convert(File inputFile, File templateFile, File outputFile, Map<String, Object> params) throws IOException, JAXBException {
         List<User> users = JaxbUtil.unmarshalAndFilter(inputFile, params).getUsers().getUser();
         ExcelPoiConverter converter = ExcelPoiConverter.of(templateFile);
 
         converter.convert(outputFile, workbook -> {
             for (int i = 0; i < users.size(); i++) {
                 User user = users.get(i);
-                workbook.cloneSheet(0);
+                Sheet sheet = workbook.cloneSheet(0);
                 workbook.setSheetName(i + 1, user.getEmail());
+                converter.resolvePlaceholders(sheet,
+                        Map.of("name", user::getName,
+                                "enabled", () -> user.isEnabled() ? "enabled" : "disabled",
+                                "email", user::getEmail,
+                                "caloriesPerDay", user::getCaloriesPerDay,
+                                // https://stackoverflow.com/questions/42210257/548473
+                                "registered", () -> DATE_TIME_FORMATTER.format(toDateTime(user.getRegistered())),
+                                "roles", () -> user.getRoles().toString())
+                );
             }
             workbook.removeSheetAt(0);
         });
